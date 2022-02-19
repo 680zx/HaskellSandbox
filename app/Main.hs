a1e39d27107acff907bd365390253beacf9c12e9
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-02-19 23:27:42 +0400

    Remove redundant functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,70 +1,27 @@
-{-# LANGUAGE MultiWayIf #-}
-
 module Main where
+import Data.Data (typeOf)
 
-import Data.List
+eqTo :: Int -> String
+eqTo 1 = "It's 1"
+eqTo _ = "Not 1"
 
-type ID = Int
-type FullName = String
-type Email = String
-type Age = Int
-type Person = (ID, FullName, Email, Age)
+func :: [(Int, Int)] -> [Int]
+func f = [t1 + t2 | (t1, t2) <- f]
 
-getEmail :: Person -> Email
-getEmail (_, _, email, _) = email
+guard :: Int -> [Double]
+guard inNum
+    | inNum <= 10 = [1, 2, 3]
+    | inNum > 10 = [11, 12, 13]
+    | otherwise = []
 
-square :: Int -> Int
-square i = i * i
+shapeSquare :: Double -> Double -> Double 
+shapeSquare length width
+    | length > 0 && width > 0 = square
+    | otherwise = 0
+    where square = length * width
 
-prod :: Int -> Int -> Int
-prod a b = a * b
-
-select :: Int -> String
-select option =
-    case option of
-        1 -> "this is 1"
-        2 -> "this is 2"
-        4 -> "this is 4"
-        _ -> "this is default"
-
-calculateTime :: Int -> Int
-calculateTime time =
-    let threshold = delta * 6
-        delta = 10
-    in
-    if | time > threshold -> time + delta
-        | time <= threshold -> time - delta
-        | otherwise -> time
-
-makeAlias :: String -> String -> (String, String)
-makeAlias str1 str2 = (str1, str2)
-
-max' :: (Ord  a) => [a] -> a
-max' [] = error "This is empty list. It cannot contain max element."
-max' [a] = a
-max' (x:xs) = max x (max' xs)
-
-
-func :: [Int] -> Int
-func a = 10
+replicate' :: Int -> Int -> [Int]
+replicate' 
 
 main :: IO ()
-main = print . max' $ [6, 2, 3]
--- main = print . (\n -> n^2) $ 2
---main = print [""]
---main = print (select (square (prod 1 2)))
---main = print (head ["1", "2"])
-{-main = print (str1, str2)
-        where (str1, str2) = makeAlias "str1" "str2"
-main = let person = getEmail (1, "New", "mail@any.com", 20)
-        in print person
--}
-
--- lambda functions
---main = print . (\x -> x * 10) $ 10
-
-{-
-    Multiline Comment
--}
-
--- Single line comment
+main = print . addToList $ 5
